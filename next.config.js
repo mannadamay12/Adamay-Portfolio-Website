3724b9695a2f5052d0d3645cbde268ce6b7a4c50
Adamay Mann <meadamann2002@gmail.com>
2024-12-26 14:24:10 -0500

    reconfig deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
     compiler: {
       styledComponents: true
     },
-    output: 'standalone'
+    // Add this to help with path resolution
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    // Add trailing slash to help with path matching
+    trailingSlash: true,
   }
   
   module.exports = nextConfig
